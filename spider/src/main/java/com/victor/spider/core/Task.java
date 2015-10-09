fb6339ce1b2f71b46058dbe8c917ae2544fa0ab3
jk983294 <jk983294@gmail.com>
2015-10-09 23:30:22 +0800

    add spider

---
+++ spider/src/main/java/com/victor/spider/core/Task.java
@@ -0,0 +1,22 @@
+package com.victor.spider.core;
+
+/**
+ * Interface for identifying different tasks.<br>
+ */
+public interface Task {
+
+    /**
+     * unique id for a task.
+     *
+     * @return uuid
+     */
+    public String getUUID();
+
+    /**
+     * site of a task
+     *
+     * @return site
+     */
+    public Site getSite();
+
+}
