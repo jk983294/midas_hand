cc6e81c3ebd1b430a28a6f154b2dd9e211e516c9
jk983294 <jk983294@gmail.com>
2015-10-26 23:12:28 +0800

    guava lib explore

--- common/src/test/java/com/victor/utilities/lib/guava/basic/BasicUsage.java
+++ common/src/test/java/com/victor/utilities/lib/guava/basic/BasicUsage.java
@@ -1,7 +1,9 @@
 package com.victor.utilities.lib.guava.basic;
 
 
-import java.util.Optional;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
@@ -10,6 +12,34 @@ import static com.google.common.base.Preconditions.checkArgument;
  */
 public class BasicUsage {
 
+    public static Integer sum(Optional<Integer> a, Optional<Integer> b) {
+        //Optional.isPresent - checks the value is present or not
+        System.out.println("First parameter is present: " + a.isPresent());
+        System.out.println("Second parameter is present: " + b.isPresent());
+
+        //Optional.or - returns the value if present otherwise returns
+        //the default value passed.
+        Integer value1 = a.or(new Integer(0));
+
+        //Optional.get - gets the value, value should be present
+        Integer value2 = b.get();
+
+        return value1 + value2;
+    }
+
+
+    public static int sum(Integer a, Integer b){
+        a = Preconditions.checkNotNull(a, "Illegal Argument passed: First parameter is Null.");
+        b = Preconditions.checkNotNull(b, "Illegal Argument passed: Second parameter is Null.");
+        return a+b;
+    }
+
+    public double sqrt(double input) throws IllegalArgumentException {
+        Preconditions.checkArgument(input > 0.0,
+                "Illegal Argument passed: Negative value %s.", input);
+        return Math.sqrt(input);
+    }
+
     /**
      * avoid null value
      */
@@ -17,6 +47,17 @@ public class BasicUsage {
         Optional<Integer> possible = Optional.of(5);
         possible.isPresent(); // returns true
         possible.get(); // returns 5
+
+        Integer value1 =  null;
+        Integer value2 =  new Integer(10);
+
+        //Optional.fromNullable - allows passed parameter to be null.
+        Optional<Integer> a = Optional.fromNullable(value1);
+
+        //Optional.of - throws NullPointerException if passed parameter is null
+        Optional<Integer> b = Optional.of(value2);
+
+        System.out.println(sum(a, b));
     }
 
     public static void preconditions(){
--- common/src/test/java/com/victor/utilities/lib/guava/basic/OrderingUsage.java
+++ common/src/test/java/com/victor/utilities/lib/guava/basic/OrderingUsage.java
@@ -7,6 +7,8 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Ordering;
 import com.google.common.primitives.Ints;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -95,11 +97,71 @@ public class OrderingUsage {
             }
         };
 
-        Ordering<Foo> ordering = Ordering.natural().nullsFirst().onResultOf(new Function<Foo, String>() {
+        Ordering<Foo> orderingObject = Ordering.natural().nullsFirst().onResultOf(new Function<Foo, String>() {
             public String apply(Foo foo) {
                 return foo.sortedBy;
             }
         });
+
+
+        List<Integer> numbers = new ArrayList<Integer>();
+
+        numbers.add(new Integer(5));
+        numbers.add(new Integer(2));
+        numbers.add(new Integer(15));
+        numbers.add(new Integer(51));
+        numbers.add(new Integer(53));
+        numbers.add(new Integer(35));
+        numbers.add(new Integer(45));
+        numbers.add(new Integer(32));
+        numbers.add(new Integer(43));
+        numbers.add(new Integer(16));
+
+        Ordering ordering = Ordering.natural();
+        System.out.println("Input List: ");
+        System.out.println(numbers);
+
+        Collections.sort(numbers,ordering );
+        System.out.println("Sorted List: ");
+        System.out.println(numbers);
+
+        System.out.println("======================");
+        System.out.println("List is sorted: " + ordering.isOrdered(numbers));
+        System.out.println("Minimum: " + ordering.min(numbers));
+        System.out.println("Maximum: " + ordering.max(numbers));
+
+        Collections.sort(numbers,ordering.reverse());
+        System.out.println("Reverse: " + numbers);
+
+        numbers.add(null);
+        System.out.println("Null added to Sorted List: ");
+        System.out.println(numbers);
+
+        Collections.sort(numbers,ordering.nullsFirst());
+        System.out.println("Null first Sorted List: ");
+        System.out.println(numbers);
+        System.out.println("======================");
+
+        List<String> names = new ArrayList<String>();
+
+        names.add("Ram");
+        names.add("Shyam");
+        names.add("Mohan");
+        names.add("Sohan");
+        names.add("Ramesh");
+        names.add("Suresh");
+        names.add("Naresh");
+        names.add("Mahesh");
+        names.add(null);
+        names.add("Vikas");
+        names.add("Deepak");
+
+        System.out.println("Another List: ");
+        System.out.println(names);
+
+        Collections.sort(names, ordering.nullsFirst().reverse());
+        System.out.println("Null first then reverse sorted list: ");
+        System.out.println(names);
     }
 
     public static void main(String[] args) {
