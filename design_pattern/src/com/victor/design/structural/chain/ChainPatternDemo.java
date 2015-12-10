8368a939f7066eace43329ea267a3f037a351c03
jk983294 <jk983294@gmail.com>
2015-12-10 22:54:29 +0800

    design pattern

---
+++ design_pattern/src/com/victor/design/structural/chain/ChainPatternDemo.java
@@ -0,0 +1,33 @@
+package com.victor.design.structural.chain;
+
+/**
+ * normally each receiver contains reference to another receiver.
+ * If one object cannot handle the request then it passes the same to the next receiver and so on.
+ */
+public class ChainPatternDemo {
+
+    private static AbstractLogger getChainOfLoggers(){
+
+        AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+        AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
+        AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
+
+        errorLogger.setNextLogger(fileLogger);
+        fileLogger.setNextLogger(consoleLogger);
+
+        return errorLogger;
+    }
+
+    public static void main(String[] args) {
+        AbstractLogger loggerChain = getChainOfLoggers();
+
+        loggerChain.logMessage(AbstractLogger.INFO,
+                "This is an information.");
+
+        loggerChain.logMessage(AbstractLogger.DEBUG,
+                "This is an debug level information.");
+
+        loggerChain.logMessage(AbstractLogger.ERROR,
+                "This is an error information.");
+    }
+}
