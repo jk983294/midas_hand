82d98e74e97a941d0f1889f8ee4ceae864c12b85
jk983294 <jk983294@gmail.com>
2015-12-20 21:57:11 +0800

    add calculator interface

---
+++ midas_server/src/main/java/com/victor/midas/calculator/common/ICalculator.java
@@ -0,0 +1,36 @@
+package com.victor.midas.calculator.common;
+
+
+import com.victor.midas.model.vo.CalcParameter;
+import com.victor.midas.model.vo.StockVo;
+import com.victor.midas.util.MidasConstants;
+import com.victor.midas.util.MidasException;
+import com.victor.midas.util.StockFilterUtil;
+
+import java.util.Set;
+
+public interface ICalculator {
+
+    public MidasConstants.CalculatorType getCalculatorType();
+
+    // aggregation use whole set to init
+    public void init_aggregation(StockFilterUtil filterUtil);
+
+    // for aggregation, pass in null since indexSH will be retrieved in StockFilterUtil
+    public void calculate(StockVo stock) throws MidasException;
+
+    public void calculate() throws MidasException;
+
+    // use this index name to register into factory
+    public String getIndexName();
+
+    public Set<String> getRequiredCalculators();
+
+    public void setRequiredCalculators();
+
+    /**
+     * for concrete calculator set their parameter
+     */
+    public void applyParameter(CalcParameter parameter);
+
+}
