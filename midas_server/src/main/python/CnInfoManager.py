6213fd45808e551fc4adcf04e1aabc87be1b17a0
jk983294 <jk983294@gmail.com>
2016-07-01 23:57:12 +0800

    add collect metadata

--- midas_server/src/main/python/CnInfoManager.py
+++ midas_server/src/main/python/CnInfoManager.py
@@ -2,6 +2,7 @@
 import PropertiesReader
 import urllib2
 import json
+import jsonpickle
 import datetime
 import requests
 import time
@@ -11,6 +12,7 @@ import re
 import codecs
 import MidasUtil as util
 import logging
+from random import randint
 
 # GET http://www.cninfo.com.cn/information/dividend/szsme002320.html
 # GET http://www.cninfo.com.cn/information/issue/szsme002320.html
@@ -20,10 +22,31 @@ import logging
 class StockData:
     stock_code = ''
     plate = ''
+    market = ''
     ipo_file = None
+    orgId = ''
 
     def __init__(self, stock_code):
         self.stock_code = stock_code
+
+    def check_stock_metadata(self):
+        if not self.orgId:
+            try:
+                time.sleep(randint(5, 25))
+                r = requests.post("http://www.cninfo.com.cn/cninfo-new/data/query",
+                                  data={"keyWord": self.stock_code}, timeout=45, stream=False)
+                if r.status_code == requests.codes.ok:
+                    result = json.loads(r.content)
+                    if len(result) > 0:
+                        self.orgId = result[0]['orgId']
+                        self.market = result[0]['market']
+                        return True
+                logging.error('download metadata failed for stock ' + self.stock_code)
+                return False
+            except (requests.exceptions.ConnectionError, requests.exceptions.RequestException):
+                logging.error('download metadata failed for stock ' + self.stock_code)
+                return False
+        return True
 
 
 class CnInfoManager:
@@ -33,33 +56,53 @@ class CnInfoManager:
     dividend_url = base_url + 'information/dividend/szsme%s.html'
     announcement_url = base_url + 'information/dividend/szsme%s.html'
     stocks = {}         # stock_code -> StockData
-
-    name = "cninfo"
-    allowed_domains = ["cninfo.com.cn"]  
-    start_urls = ["http://www.cninfo.com.cn/cninfo-new/announcement/show"]
-    stockCodeSumNum = 2
-    homePage = r"http://www.cninfo.com.cn"
-    savedStockSumNum = 0
+    current_stock = None
+    allowed_domains = ["cninfo.com.cn"]
 
     def __init__(self, base_path):
         self.base_path = base_path
+        self.deserialization_stock_data()
         self.collect_disk_data()
-        log_path = self.base_path + "log/log_" + time.strftime("%Y%m%d_%H%M%S", time.localtime()) + ".txt"
+        log_path = self.base_path + "log/log_" + time.strftime("%Y%m%d_%H_%M_%S", time.localtime()) + ".txt"
         logging.basicConfig(filename=log_path, level=logging.DEBUG)
         logging.info('start log...')
 
-    def collect_data_from_stock_dir(self, stock_code):
-        stock_data = StockData(stock_code)
-        folder = self.base_path + stock_code
+    def serialization_stock_data(self):
+        serialization_path = self.base_path + 'stocks.json'
+        f = open(serialization_path, 'w')
+        jsonpickle.set_encoder_options('simplejson', sort_keys=True, indent=4)
+        f.write(jsonpickle.encode(self.stocks))
+        f.close()
+
+    def deserialization_stock_data(self):
+        serialization_path = self.base_path + 'stocks.json'
+        if os.path.exists(serialization_path):
+            f = open(serialization_path, 'r')
+            obj = jsonpickle.decode(f.read())
+            self.stocks = obj
+
+    def collect_data_from_stock_dir(self):
+        folder = self.base_path + self.current_stock.stock_code
         if not os.path.exists(folder):
             os.mkdir(folder)
-        return stock_data
 
     def collect_disk_data(self):
         for code in util.get_all_stock_codes():
             if code is not None and not code.startswith('IDX'):
                 stock_code = code[2:]
-                self.stocks[stock_code] = self.collect_data_from_stock_dir(stock_code)
+                if stock_code not in self.stocks:
+                    self.current_stock = StockData(stock_code)
+                    self.stocks[stock_code] = self.current_stock
+                    self.collect_data_from_stock_dir()
+                else:
+                    self.current_stock = self.stocks[stock_code]
+                self.current_stock.check_stock_metadata()
+
+    def set_current_stock(self, stock_code):
+        if stock_code in self.stocks:
+            self.current_stock = None
+        else:
+            self.current_stock = self.stocks[stock_code]
 
     def download_price_zip(self, store_location, download_url):
         # pdfPath = self.base_path + '/' + file_name + '.pdf'
@@ -67,31 +110,13 @@ class CnInfoManager:
         # print pdfPath, realURL
         try:
             r = requests.post(download_url,
-                              # headers={
-                              #     'Accept-encoding': 'gzip, deflate',
-                              #     'Host': 'www.cninfo.com.cn',
-                              #     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                              #     'Content-Type': 'multipart/form-data',
-                              #     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36',
-                              #     'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4'
-                              # },
-                              # data={
-                              #     "market": "sz",
-                              #     "type": "hq",
-                              #     "code": "000001",
-                              #     "minYear": "2005",
-                              #     "maxYear": "2016",
-                              #     "hq_code": "000001",
-                              #     "orgid": "gssz0000001"
-                              # },
                               files={
-                                  "market": (None, "sz"),
+                                  "market": (None, self.current_stock.market),
                                   "type": (None, "hq"),
-                                  "code": (None, "000001"),
-                                  "minYear": (None, "2005"),
+                                  "code": (None, self.current_stock.stock_code),
+                                  "minYear": (None, "2000"),
                                   "maxYear": (None, "2016"),
-                                  "hq_code": (None, "000001"),
-                                  "orgid": (None, "gssz0000001")
+                                  "orgid": (None, self.current_stock.orgId)
                               }
                               )
             file_obj = open(store_location, 'w')
@@ -121,5 +146,7 @@ if __name__ == '__main__':
     my_props = PropertiesReader.get_properties()
     manager = CnInfoManager(my_props['MktDataLoader.Fundamental.cninfo'])
     # manager.download_pdf('', '')
+    manager.set_current_stock('000001')
     manager.download_price_zip('F:/Data/MktData/fundamental/cninfo/000001/sz_hq_000001_2005_2016.zip', 'http://www.cninfo.com.cn/cninfo-new/data/download')
+    manager.serialization_stock_data()
     print 'download class info finished'
